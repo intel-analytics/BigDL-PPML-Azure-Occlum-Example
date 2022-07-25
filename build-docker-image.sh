ec885eff6acf28fc2f3b8487740274cbffbff04e
Xiangyu Tian <oveoveove142@outlook.com>
2022-07-25 16:35:07 +0800

    Build the maa_init demo of occlum

---
+++ build-docker-image.sh
@@ -0,0 +1,13 @@
+export HTTP_PROXY_HOST=child-prc.intel.com
+export HTTP_PROXY_PORT=913
+export HTTPS_PROXY_HOST=child-prc.intel.com
+export HTTPS_PROXY_PORT=913
+
+sudo docker build \
+    --build-arg http_proxy=http://$HTTP_PROXY_HOST:$HTTP_PROXY_PORT \
+    --build-arg https_proxy=http://$HTTPS_PROXY_HOST:$HTTPS_PROXY_PORT \
+    --build-arg HTTP_PROXY_HOST=$HTTP_PROXY_HOST \
+    --build-arg HTTP_PROXY_PORT=$HTTP_PROXY_PORT \
+    --build-arg HTTPS_PROXY_HOST=$HTTPS_PROXY_HOST \
+    --build-arg HTTPS_PROXY_PORT=$HTTPS_PROXY_PORT \
+    -t intelanalytics/bigdl-ppml-azure-attestation-occlum:2.1.0-SNAPSHOT -f ./Dockerfile .
