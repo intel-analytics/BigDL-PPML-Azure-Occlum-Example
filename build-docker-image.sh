ade1bb85810d842b2277e0e564f7f6507d05c90d
Xiangyu Tian <109123695+xiangyuT@users.noreply.github.com>
2022-08-05 10:31:28 +0800

    Integrate NYTaxi with current image (#7)
    * Integrate spark-simple-query with current image
    * Fix some problems
    
    Co-authored-by: xiangyuT <xiangyu.tian@intel.com>

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -3,6 +3,10 @@ export HTTP_PROXY_PORT=913
 export HTTPS_PROXY_HOST=child-prc.intel.com
 export HTTPS_PROXY_PORT=913
 
+pushd nytaxi
+mvn package 
+popd
+
 sudo docker build \
     --build-arg http_proxy=http://$HTTP_PROXY_HOST:$HTTP_PROXY_PORT \
     --build-arg https_proxy=http://$HTTPS_PROXY_HOST:$HTTPS_PROXY_PORT \
